cffa77eed27c4335d405236bc21d9498a180fbda
ZenithSlayer <miguellurk+github@gmail.com>
2026-03-13 12:00:32 -0300

    update :D

--- pages/login.php
+++ pages/login.php
@@ -4,7 +4,16 @@
             <img src="../imgs/icon.png" alt="">
         </div>
         <form class="loginForm">
-
+            <p class="title">Login</p>
+            <div class="inputSection">
+                <p>Email/Name</p>
+                <input type="text" name="email" id="email">
+            </div>
+            <div class="inputSection">
+                <p>Password</p>
+                <input type="text" name="password" id="password">
+            </div>
+            <input class="submit" type="submit" value="Yeah, thats me">
         </form>
     </div>
 </div>
@@ -32,9 +41,9 @@
             flex-direction: row;
 
             .decoration {
-                background-color: rgb(53 181 175);
+                background-color: var(--light-cyan);
                 width: 60px;
-                height: 300px;
+                height: align-items;
                 display: flex;
                 justify-content: center;
                 align-items: end;
@@ -49,9 +58,48 @@
                 }
             }
 
+
             .loginForm {
-                width: 200px;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                border-radius: 0 10px 10px 0;
+                padding: 20px 40px;
                 background-color: gray;
+
+                .title {
+                    text-align: center;
+                    font-weight: bold;
+                    font-size: 30px;
+                    margin: 0 0 15px 0;
+                }
+
+                .inputSection {
+                    margin: 0 0 10px 0;
+
+                    p {
+                        margin: 0 0 5px 0;
+                        font-size: 1.3em;
+                        font-weight: 500;
+                        color: var(--dark-gray);
+                    }
+
+                    input {
+                        font-size: 20px;
+                        border: 0;
+                        border-radius: 5px;
+                        padding: 5px;
+                    }
+                }
+
+                .submit {
+                    margin: 10px 0;
+                    border: none;
+                    padding: 5px 0;
+                    width: 80%;
+                    border-radius: 5px;
+                    background-color: var(--light-cyan);
+                }
             }
         }
     }
